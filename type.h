39ba14946ba3aed8a3f4b5aa55a8c5d411f0c55c
Ken <kenteramoto4@gmail.com>
2026-09-10 20:45:07 +0700

    Added structure

---
+++ type.h
@@ -0,0 +1,33 @@
+#ifndef TYPES_H
+#define TYPES_H
+#include <string>
+using namespace std;
+
+struct Category {
+    int id;
+    string name;
+};
+
+struct Product {
+    int id;
+    string name;
+    int categoryId;
+    double price;
+    int stock;
+};
+
+struct Customer {
+    int id;
+    string name;
+    string phone;
+};
+
+struct Order {
+    int id;
+    int customerId;
+    int productId;
+    int quantity;
+    string date;
+};
+
+#endif
